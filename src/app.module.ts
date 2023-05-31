0e0e94a2bb77a3361210f292be9a0cd355824152
Mahmudul <mahmudulhaqshaown@gmail.com>
2023-05-31 16:11:09 +0600

    Fixed an issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { config } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-import { AddUserModule } from './modules/add-user/add-user.module';
 import { UserModule } from './modules/user/user.module';
 import { JwtAuthGuard } from './modules/guard/jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
@@ -19,7 +18,6 @@ import { UserProfileModule } from './modules/user-profile/user-profile.module';
       signOptions: { expiresIn: '1d' },
     }),
     AuthModule,
-    AddUserModule,
     UserModule,
     UserProfileModule,
   ],
